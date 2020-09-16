92f2b2b30dec30e40dd1bad17667b00aa72db38a
Vy Le <lethaovy2001@gmail.com>
2020-09-16 16:54:05 -0500

    create project

---
+++ app/src/main/java/com/example/simplyrecipes/Activities/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.simplyrecipes.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.simplyrecipes.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
