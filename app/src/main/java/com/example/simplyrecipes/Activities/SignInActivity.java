e03decfac9452448c90615cd6201e13ff9828e49
Vy Le <lethaovy2001@gmail.com>
2020-10-16 15:25:20 -0500

    finish layout for Sign In

---
+++ app/src/main/java/com/example/simplyrecipes/Activities/SignInActivity.java
@@ -0,0 +1,17 @@
+package com.example.simplyrecipes.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.simplyrecipes.R;
+
+public class SignInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+    }
+
+
+}
