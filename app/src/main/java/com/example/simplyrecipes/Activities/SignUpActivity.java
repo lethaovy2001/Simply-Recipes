bf19f17f9915e28b10defaadf3b96e703f1abc85
Vy Le <lethaovy2001@gmail.com>
2020-10-17 16:15:38 -0500

    finish layout for Sign Up

---
+++ app/src/main/java/com/example/simplyrecipes/Activities/SignUpActivity.java
@@ -0,0 +1,15 @@
+package com.example.simplyrecipes.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.simplyrecipes.R;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+}
