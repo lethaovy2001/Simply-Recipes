d1da789caec76d35a96d4fb20b32520dfec2cce2
krohyunjae <yroh3@wisc.edu>
2020-10-30 22:06:15 -0500

    updated gradle for unit testing

---
+++ app/src/test/java/com/example/simplyrecipes/Activities/MainActivityTest.java
@@ -0,0 +1,16 @@
+package com.example.simplyrecipes.Activities;
+
+class MainActivityTest {
+//    @Rule
+//    public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<MainActivity>(MainActivity.class);
+//    @org.junit.jupiter.api.BeforeEach
+//    void setUp() {
+//    }
+//    @Test
+//    void testLaunch(){
+//
+//    }
+//    @org.junit.jupiter.api.AfterEach
+//    void tearDown() {
+//    }
+}
