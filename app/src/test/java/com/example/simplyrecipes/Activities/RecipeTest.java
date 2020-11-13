7ab34e4f6aa7b909e0ed87bfe31d42c61b835460
krohyunjae <yroh3@wisc.edu>
2020-11-12 20:34:22 -0600

    More unit tests

---
+++ app/src/test/java/com/example/simplyrecipes/Activities/RecipeTest.java
@@ -0,0 +1,67 @@
+package com.example.simplyrecipes.Activities;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RecipeTest {
+
+    @BeforeEach
+    void setUp() {
+
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getRecipeTime() {
+    }
+
+    @Test
+    void setRecipeTime() {
+    }
+
+    @Test
+    void getRecipeRating() {
+    }
+
+    @Test
+    void setRecipeRating() {
+    }
+
+    @Test
+    void getTitle() {
+    }
+
+    @Test
+    void setTitle() {
+    }
+
+    @Test
+    void getImage() {
+    }
+
+    @Test
+    void getRecipeID() {
+    }
+
+    @Test
+    void setRecipeID() {
+    }
+
+    @Test
+    void setImage() {
+    }
+
+    @Test
+    void getUrl() {
+    }
+
+    @Test
+    void setUrl() {
+    }
+}
